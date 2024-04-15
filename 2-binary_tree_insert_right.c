dc5a7599195de1dcb72d2b8ae350464d262a175e
TshidisoMp3 <tshidisomorake18@gmail.com>
2024-04-15 23:55:03 +0200

    Binary_Trees

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -7,7 +7,7 @@
     * Return: pointer to the new node or NULL on failure
     */
 
-binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
     binary_tree_t *new_node = NULL;
 
@@ -29,4 +29,4 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
     }
 
     parent->right = new_node;
-}
+};
