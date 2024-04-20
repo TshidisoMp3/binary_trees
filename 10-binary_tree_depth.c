c791b14d2a56d73c9a24a1dda7670a397245af2b
TshidisoMp3 <tshidisomorake18@gmail.com>
2024-04-20 21:24:23 +0200

    Binary_Trees

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,7 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - measures the depth of a node in a binary tree
+ * @tree: pointer to the node to measure the depth
+ * Return: depth of the node
+*/
