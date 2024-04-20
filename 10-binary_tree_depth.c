f96324f09b06b9aa56a166244cd43045d21ffcf4
TshidisoMp3 <tshidisomorake18@gmail.com>
2024-04-20 21:27:28 +0200

    Binary_Trees

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -5,3 +5,16 @@
  * @tree: pointer to the node to measure the depth
  * Return: depth of the node
 */
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+    size_t depth = 0;
+
+    if (tree == NULL)
+        return (0);
+
+    if (tree->parent)
+        depth = 1 + binary_tree_depth(tree->parent);
+
+    return (depth);
+}
